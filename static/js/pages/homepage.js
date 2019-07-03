81f8c06f684c85cfa9609149c674bc8bff2a28c6
LaupWing <loc_strict@hotmail.com>
2019-07-03 23:03:05 +0200

    update

--- static/js/pages/homepage.js
+++ static/js/pages/homepage.js
@@ -5,7 +5,6 @@ async function requestHomeFeed (){
     const homefeed = await fetch('http://localhost:3001/homefeed')
     const html     = await homefeed.text()
     const container = document.querySelector('section#homeFeed')
-    console.log(html)
     removeChilds(container)
     container.insertAdjacentHTML('beforeend', html)
     feed.iframeActivate()

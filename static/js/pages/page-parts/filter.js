f2ee66c9fdf7bd8b80671d28b49ae700fb96b427
LaupWing <loc_strict@hotmail.com>
2019-07-09 21:25:45 +0200

    fixed filter functions

--- static/js/pages/page-parts/filter.js
+++ static/js/pages/page-parts/filter.js
@@ -1,6 +1,10 @@
+let prevConfig = null
+let allPosts = null
 function event(){
-    console.log('start')
     document.querySelector('.filter-btn').addEventListener('click', showFilters)
+    prevConfig = checkFilters()
+    allPosts = Array.from(document.querySelectorAll('#feed .iframe-wrapper'))
+    console.log(prevConfig, allPosts)
 }
 
 function showFilters(){
@@ -10,6 +14,7 @@ function showFilters(){
         btnEvents('remove')
         document.querySelector('.filter-screen').classList.remove('reveal')
         this.classList.remove('open')
+        adjustContent()
         this.querySelector('img').src = '/img/filter.png'
     }else{
         document.querySelector('.filter-screen').classList.add('reveal')
@@ -32,21 +37,73 @@ function btnEvents(state){
 
 function checkFilters(){
     const valueBy = document.querySelector('input[name="by"]:checked').value
-    const valueOrder = document.querySelector('input[name="order"]:checked').value
     const keywords = Array.from(document.querySelectorAll('.keywords button'))
         .filter(btn=>btn.classList.contains('active'))
-        .map(btn=>btn.textContent)
+        .map(btn=>btn.textContent.toLocaleLowerCase())
     const config = {
         valueBy,
-        valueOrder,
         keywords
     }
     return config
 }
 
+
 function adjustContent(){
     const config = checkFilters()
+    let posts = null
+    if(allPosts!==null){
+        posts = allPosts 
+    }else{
+        posts = Array.from(document.querySelectorAll('#feed .iframe-wrapper'))
+    }
+    const filtered = posts.filter(post=>{
+        console.log(post)
+        for(let keyword of config.keywords){
+            console.log(keyword)
+            if(keyword===post.dataset.platform) return post
+        }
+    })
+    if(config.valueBy === 'recent'){
+        const sortedAndFilterd = filtered.sort((a,b)=>{
+            if (a.dataset.timestamp > b.dataset.timestamp) {
+                return -1;
+            }
+            if (a.dataset.timestamp < b.dataset.timestamp) {
+                return 1;
+            }
+            // a must be equal to b
+            return 0;
+        })
+        reRenderFeed(sortedAndFilterd)
+    }else{
+        const sortedAndFilterd = filtered.sort((a,b)=>{
+            if (a.dataset.timestamp < b.dataset.timestamp) {
+                return -1;
+            }
+            if (a.dataset.timestamp > b.dataset.timestamp) {
+                return 1;
+            }
+            // a must be equal to b
+            return 0;
+        })
+        reRenderFeed(sortedAndFilterd)
+    }
+}
 
+function reRenderFeed(posts){
+    removePosts()
+    const container = document.querySelector('#feed')
+    posts.forEach(post=>{
+        console.log(post)
+        container.insertAdjacentElement('beforeend', post)
+    })
+}
+function removePosts(){
+    const container = document.querySelector('#feed')
+    if(container===null)    return
+    while(container.querySelector('.iframe-wrapper')){
+        container.removeChild(container.querySelector('.iframe-wrapper'))
+    }
 }
 
 export {event}
